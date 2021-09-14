41e2563ff07077eab1cd4c00b2e6cb3630d18148
Duc1104 <87553948+Duc1104@users.noreply.github.com>
2021-09-14 19:22:15 +0700

    Update and rename README.md to Nhiet_do_trung_binh.cpp

---
+++ Nhiet_do_trung_binh.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+Using namespace std;
+ Int main(){
+ Float a,b,c;
+   Cout<<"nhiet do sang"<<a<<endl;
+   Cout<<"nhiet do trua"<<b<<endl;
+   Cout<<"nhiet do toi"<<c<<endl;
+   Cin>>a>>b>>c;
+   Float d;
+   d=(a+b+c)/3;
+   Cout<<"nhiet do trung binh ngay la"<<d<<endl;
+Return 0;
+}
